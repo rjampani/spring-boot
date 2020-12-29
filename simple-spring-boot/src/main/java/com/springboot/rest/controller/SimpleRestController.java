29bc114e282fa5176dc0f98e1dd7d59ca34e24af
rjampani <jampany.ravindra@gmail.com>
2020-12-28 21:28:34 -0800

    Update SimpleRestController.java
    changed url rest/simple to api/simple

--- simple-spring-boot/src/main/java/com/springboot/rest/controller/SimpleRestController.java
+++ simple-spring-boot/src/main/java/com/springboot/rest/controller/SimpleRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/simple")
+@RequestMapping("/api/simple")
 public class SimpleRestController {
 	
 	@GetMapping
